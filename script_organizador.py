d368a99b6e42cffe360c9fc2a2a0f0ffb33314a4
Mateo Arango <matarango96@gmail.com>
2023-05-15 01:44:25 -0500

    Se agregan los archivos comprimidos

--- script_organizador.py
+++ script_organizador.py
@@ -4,16 +4,16 @@ import shutil
 def main():
     imagenes = ('bmp', 'gif', 'jpg', 'jpeg', 'jpe', 'jfif', 'tif','tiff', 'png')
     audio = ('wav', 'wave', 'aiff', 'pcm', 'flac', 'm4a','wma', 'wmv', 'mp3', 'ogg', 'aac')
-    ejecutables = ('exe')
-    pdf = ('pdf')
+    ejecutables = ('exe', 'py')
+    pdf = ('pdf', 'PDF')
     office = ('doc', 'docx', 'rtf', 'xls', 'xlsx', 'ppt', 'pptx')
+    comprimidos = ('zip', 'rar', 'tar')
 
     # Creamos carpetas
     carpetas()
 
     # Listamos los archivos en el directorio actual
     archivos = os.listdir()
-
 
     for archivo in archivos:
         if os.path.isfile(archivo):
@@ -25,19 +25,18 @@ def main():
                 mover(archivo, 'Downloaded Music')
             elif extension in pdf:
                 mover(archivo, 'Downloaded PDFs')
-            elif extension in ejecutables:
+            elif extension in ejecutables and archivo.split('.')[0] != os.path.basename(__file__).split('.')[0]:
+                # mueve cualquier archivo ejecutable que no sea el mismo
                 mover(archivo, 'Downloaded Executables')
             elif extension in office:
                 mover(archivo, 'Downloaded Office Files')
-
-
-
-
-
+            elif extension in comprimidos:
+                mover(archivo, 'Downloaded Compressed Files')
 
 def carpetas():
 
-    directorios = ('Downloaded Pictures','Downloaded Music','Downloaded PDFs','Downloaded Executables', 'Downloaded Office Files')
+    directorios = ('Downloaded Pictures','Downloaded Music','Downloaded PDFs','Downloaded Executables', 'Downloaded Office Files',
+                   'Downloaded Compressed Files')
     archivos = os.listdir()
 
     for i in directorios:
@@ -52,10 +51,6 @@ def carpetas():
 def mover(archivo, dest):
     # mueve un archivo desde la carpeta actual a la carpeta "dest"
     shutil.move(archivo, dest + '/' +  archivo)
-
-
-
-
 
 if __name__ == '__main__':
     main()
